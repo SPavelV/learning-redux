10d78122bcdba16993bd2edd5d134d3b182f177d
Pavel Solovev <pashius2010@gmail.com>
2021-02-27 23:20:01 +0400

    initial commit

---
+++ source/basic-redux/bus/gallery/reducer.js
@@ -0,0 +1,33 @@
+import imageUrl1 from "../../../theme/assets/images/1.jpeg";
+import imageUrl2 from "../../../theme/assets/images/2.jpeg";
+import imageUrl3 from "../../../theme/assets/images/3.jpeg";
+import imageUrl4 from "../../../theme/assets/images/4.jpeg";
+
+// Types
+import { SHOW_NEXT_PHOTO } from "./types";
+
+const initialState = {
+  photos: [
+    { id: "1", url: imageUrl1 },
+    { id: "2", url: imageUrl2 },
+    { id: "3", url: imageUrl3 },
+    { id: "4", url: imageUrl4 },
+  ],
+  selectedPhotoIndex: 0,
+};
+
+export const galleryReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SHOW_NEXT_PHOTO:
+      if (state.selectedPhotoIndex === state.photos.length - 1) {
+        return state;
+      }
+      return {
+        ...state,
+        selectedPhotoIndex: state.selectedPhotoIndex + 1,
+      };
+
+    default:
+      return state;
+  }
+};
---
+++ source/basic-redux/components/Gallery.js
@@ -0,0 +1,50 @@
+// Core
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+import cx from "classnames";
+
+// Components
+import { Button } from "./Button";
+
+// Instruments
+import Styles from "./styles.m.css";
+
+// Store
+import { store } from "../init/store";
+
+// Actions
+import { showNextPhoto } from "../bus/gallery/actions";
+
+@hot(module)
+export class Gallery extends Component {
+  _showNextPhoto = () => {
+    store.dispatch(showNextPhoto);
+    this.forceUpdate();
+  };
+
+  render() {
+    const {
+      gallery: { photos, selectedPhotoIndex },
+    } = store.getState();
+    const photo = photos.find((_, index) => index === selectedPhotoIndex);
+
+    const buttons = [...Array(4).keys()].map((_, currentImageIndex) => (
+      <Button
+        key={currentImageIndex}
+        currentImageIndex={currentImageIndex}
+        selectedImageIndex={selectedPhotoIndex}
+      />
+    ));
+
+    return (
+      <section className={Styles.gallery}>
+        <img src={photo.url} />
+        <div>
+          <button>←</button>
+          {buttons}
+          <button onClick={this._showNextPhoto}>→</button>
+        </div>
+      </section>
+    );
+  }
+}
