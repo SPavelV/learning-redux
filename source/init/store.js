487665edfaa3f9d7b2b07a4919624e32a5e876b4
Pavel Solovev <pashius2010@gmail.com>
2021-03-01 01:13:10 +0400

    advanced concepts redux

---
+++ source/init/store.js
@@ -0,0 +1,24 @@
+// Core
+import { createStore, applyMiddleware, compose } from "redux";
+import { createLogger } from "redux-logger";
+
+// Reducer
+import { rootReducer } from "./rootReducer";
+
+const logger = createLogger({
+  duration: true,
+  collapsed: true,
+  colors: {
+    title: () => "#139bfe",
+    prevState: () => "#1c5faf",
+    action: () => "#149945",
+    nextState: () => "#A47104",
+    error: () => "#ff0005",
+  },
+});
+
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = devtools ? devtools : compose;
+const enhacedStore = composeEnhancers(applyMiddleware(logger));
+
+export const store = createStore(rootReducer, enhacedStore);
--- source/instruments/mockedData.js
+++ source/instruments/mockedData.js
@@ -1,9 +1,9 @@
 // Core
-import { Map } from 'immutable';
+import { Map } from "immutable";
 
 export const mockedProfile = Map({
-    firstName: 'Walter',
-    lastName:  'White',
-    avatar:
-        'https://orig00.deviantart.net/8459/f/2016/186/2/c/heisenberg_portrait_by_yannickbouchard-da8twpb.jpg',
+  firstName: "Walter",
+  lastName: "White",
+  avatar:
+    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fddb327b-8d6b-45b0-8a1e-0cb553532f4a/da8twpb-0dff08f1-f042-4887-be3d-023f6f83187b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZmRkYjMyN2ItOGQ2Yi00NWIwLThhMWUtMGNiNTUzNTMyZjRhXC9kYTh0d3BiLTBkZmYwOGYxLWYwNDItNDg4Ny1iZTNkLTAyM2Y2ZjgzMTg3Yi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.bAQ1ou35jl4S1SHGQ2ivbH5axOoveALfkskZTSD9FXk",
 });
