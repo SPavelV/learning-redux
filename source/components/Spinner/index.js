035a80d68dc832e90d97d314c27d060fbe4b08ac
Pavel Solovev <pashius2010@gmail.com>
2021-03-06 13:22:00 +0400

    spinner state

--- source/components/Spinner/index.js
+++ source/components/Spinner/index.js
@@ -1,17 +1,21 @@
 // Core
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 // Instruments
-import Styles from './styles.m.css';
+import Styles from "./styles.m.css";
 
+const mapStateToProps = (state) => {
+  return {
+    isFetching: state.ui.get("isFetching"),
+  };
+};
+
+@connect(mapStateToProps)
 export class Spinner extends Component {
-    static defaultProps = {
-        isFetching: false,
-    };
+  render() {
+    const { isFetching } = this.props;
 
-    render() {
-        const { isFetching } = this.props;
-
-        return isFetching ? <div className = { Styles.spinner } /> : null;
-    }
+    return isFetching ? <div className={Styles.spinner} /> : null;
+  }
 }
