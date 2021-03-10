eb34fd2bcc502a065fde03275b10689f38ca97a4
Pavel Solovev <pashius2010@gmail.com>
2021-03-10 08:51:30 +0400

    authentication

--- source/components/Header/index.js
+++ source/components/Header/index.js
@@ -1,77 +1,83 @@
 // Core
-import React, { Component } from 'react';
-import { NavLink, withRouter } from 'react-router-dom';
-import cx from 'classnames';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
+import cx from "classnames";
 
 // Instruments
-import Styles from './styles.m.css';
-import { book } from '../../routes/book';
-import { mockedProfile } from '../../instruments/mockedData';
+import Styles from "./styles.m.css";
+import { book } from "../../routes/book";
+import { mockedProfile } from "../../instruments/mockedData";
 
-@withRouter
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.get("isAuthenticated"),
+  };
+};
+
+@connect(mapStateToProps)
 export class Header extends Component {
-    static defaultProps = {
-        // State
-        profile: mockedProfile,
-        isAuthenticated: true,
-        isOnline: false,
+  static defaultProps = {
+    // State
+    profile: mockedProfile,
+    isOnline: false,
 
-        // Actions
-        logoutAsync: () => {},
-    };
+    // Actions
+    logoutAsync: () => {},
+  };
 
-    _getNav = () => {
-        const { isAuthenticated, profile } = this.props;
+  _getNav = () => {
+    const { isAuthenticated, profile } = this.props;
 
-        return isAuthenticated ? (
-            <>
-                <div>
-                    <NavLink activeClassName={Styles.active} to={book.profile}>
-                        <img src={profile.get('avatar')} />
-                        {profile.get('firstName')}
-                    </NavLink>
-                    <NavLink activeClassName={Styles.active} to={book.feed}>
-                        Стена
-                    </NavLink>
-                </div>
-                <button onClick={this._logout}>Выйти</button>
-            </>
-        ) : (
-            <>
-                <div>
-                    <NavLink activeClassName={Styles.active} to={book.login}>
-                        Войти
-                    </NavLink>
-                    <NavLink activeClassName={Styles.active} to={book.signUp}>
-                        Создать аккаунт
-                    </NavLink>
-                </div>
-                <button className={Styles.hidden}>Выйти</button>
-            </>
-        );
-    };
+    return isAuthenticated ? (
+      <>
+        <div>
+          <NavLink activeClassName={Styles.active} to={book.profile}>
+            <img src={profile.get("avatar")} />
+            {profile.get("firstName")}
+          </NavLink>
+          <NavLink activeClassName={Styles.active} to={book.feed}>
+            Стена
+          </NavLink>
+        </div>
+        <button onClick={this._logout}>Выйти</button>
+      </>
+    ) : (
+      <>
+        <div>
+          <NavLink activeClassName={Styles.active} to={book.login}>
+            Войти
+          </NavLink>
+          <NavLink activeClassName={Styles.active} to={book.signUp}>
+            Создать аккаунт
+          </NavLink>
+        </div>
+        <button className={Styles.hidden}>Выйти</button>
+      </>
+    );
+  };
 
-    _logout = () => {
-        this.props.logoutAsync();
-    };
+  _logout = () => {
+    this.props.logoutAsync();
+  };
 
-    render() {
-        const { isOnline } = this.props;
+  render() {
+    const { isOnline } = this.props;
 
-        const navigation = this._getNav();
-        const statusStyle = cx(Styles.status, {
-            [Styles.online]: isOnline,
-            [Styles.offline]: !isOnline,
-        });
+    const navigation = this._getNav();
+    const statusStyle = cx(Styles.status, {
+      [Styles.online]: isOnline,
+      [Styles.offline]: !isOnline,
+    });
 
-        return (
-            <header className={Styles.header}>
-                <div className={statusStyle}>
-                    <div>{isOnline ? 'Онлайн' : 'Офлайн'}</div>
-                    <span />
-                </div>
-                {navigation}
-            </header>
-        );
-    }
+    return (
+      <header className={Styles.header}>
+        <div className={statusStyle}>
+          <div>{isOnline ? "Онлайн" : "Офлайн"}</div>
+          <span />
+        </div>
+        {navigation}
+      </header>
+    );
+  }
 }
